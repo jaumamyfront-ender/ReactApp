549979de7f98b243cda9abc9fb3265f1c2f05017
JavaScriptFromUkraine <nepobedimyjpobedit@gmail.com>
2023-10-11 13:05:48 -0700

    buildGridStructure

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-   
+    <div className="containerGrid">
+      <div className="header"></div>
+      <div className="sidebar"></div>
+      <div className="content"></div>
+    </div>
   );
 }
 
