8e257eb7f612014ee2b8fe7bcbf8fa921738268d
JavaScriptFromUkraine <nepobedimyjpobedit@gmail.com>
2023-10-11 16:30:58 -0700

    BuildHeaderAndSide

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,42 @@
 import "./App.css";
+import raster from "./assets/Raster.png";
 
 function App() {
   return (
     <div className="containerGrid">
-      <div className="header"></div>
-      <div className="sidebar"></div>
-      <div className="content"></div>
+      <div className="header">
+        <div className="headerImage">
+          <img src={raster} alt="/#"></img>
+        </div>
+      </div>
+      <div className="sidebar">
+        <div className="sidebar__list">
+          {" "}
+          <a href="/#">Profile</a>
+          <a href="/#">Messages</a>
+          <a href="/#">News</a>
+          <a href="/#">Music</a>
+          <a href="/#">Settings</a>
+        </div>
+        <div className="sidebar__friends">
+          <a href="/#" alt="/#" className="qqq">
+            Friends
+          </a>
+          <div className="userspictures one">
+            <img></img>
+          </div>
+          <div className="userspictures second">
+            <img></img>
+          </div>
+          <div className="userspictures three">
+            <img></img>
+          </div>
+          <div className="users__name">sdfsd</div>
+          <div className="users__name">fdsfs</div>
+          <div className="users__name">fdsfsd</div>
+        </div>
+      </div>
+      <div className="content">content</div>
     </div>
   );
 }
