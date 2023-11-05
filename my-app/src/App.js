30c04542123f231d0a63f6bf14864c388440a1ef
JavaScriptFromUkraine <nepobedimyjpobedit@gmail.com>
2023-11-05 20:17:30 +0100

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,15 +3,15 @@ import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Dialogs from "./components/Dialogs/Dialogs.jsx";
-// import MainRoutes from "./components/Routes/Routes";
+import MainRoutes from "./components/Routes/Routes";
 
 function App() {
   return (
     <div className="container">
       <Header />
       <Sidebar />
-      {/* <MainRoutes /> */}
-      <Dialogs />
+      <MainRoutes />
+      {/* <Dialogs /> */}
     </div>
   );
 }
