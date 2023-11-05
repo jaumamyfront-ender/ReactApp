89e48c7599c547299858e737c740507b48d0cdb5
JavaScriptFromUkraine <nepobedimyjpobedit@gmail.com>
2023-11-05 17:57:36 +0100

    writeDialogs

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -1,25 +1,59 @@
 import React from "react";
 import classes from "./dialogs.module.css";
+import { NavLink } from "react-router-dom";
+const setActive = ({ isActive }) => (isActive ? "active__link" : "");
 
 export default function Dialogs() {
   return (
     <div className="Wrapper">
       <div className={classes.Container}>
-        <div className={classes.Head}></div>
-        <div className={`${classes.SideBar} ${classes.Left}`}>
-          <div className={`${classes.Item} ${classes.one}`}></div>
-          <div className={`${classes.Item} ${classes.second}`}></div>
-          <div className={`${classes.Item} ${classes.three}`}></div>
-          <div className={`${classes.Item} ${classes.third}`}></div>
-          <div className={`${classes.Item} ${classes.five}`}></div>
-          <div className={`${classes.Item} ${classes.six}`}></div>
-          <div className={`${classes.Item} ${classes.seven}`}></div>
-          <div className={`${classes.Item} ${classes.eight}`}></div>
-          <div className={`${classes.Item} ${classes.nine}`}></div>
-          <div className={`${classes.Item} ${classes.teen}`}></div>
+        <div className={classes.Head}>
+          <p>Dialogs</p>
+        </div>
+        <div className={`${classes.SideBar} ${classes.LeftWrapper}`}>
+          <NavLink to="/#" alt="/#" className={classes.sidebar__items}>
+            Andrew
+          </NavLink>
+          <NavLink to="/#" alt="/#" className={classes.sidebar__items}>
+            Friends
+          </NavLink>
+          <NavLink to="/#" alt="/#" className={classes.sidebar__items}>
+            Friends
+          </NavLink>
+          <NavLink to="/#" alt="/#" className={classes.sidebar__items}>
+            Friends
+          </NavLink>
+          <NavLink to="/#" alt="/#" className={classes.sidebar__items}>
+            Friends
+          </NavLink>
+          <NavLink to="/#" alt="/#" className={classes.sidebar__items}>
+            Friends
+          </NavLink>
+          <NavLink to="/#" alt="/#" className={classes.sidebar__items}>
+            Friends
+          </NavLink>
+          <NavLink to="/#" alt="/#" className={classes.sidebar__items}>
+            Friends
+          </NavLink>
+          <NavLink to="/#" alt="/#" className={classes.sidebar__items}>
+            Friends
+          </NavLink>
+          <NavLink to="/#" alt="/#" className={classes.sidebar__items}>
+            Friends
+          </NavLink>
         </div>
         <div className={classes.Barrier}></div>
-        <div className={`${classes.SideBar} ${classes.Right}`}></div>
+        <div className={`${classes.SideBar} ${classes.RightWrapper}`}>
+          <div className={classes.rightWrapper__items}>
+            <div className={classes.user__info}>
+              <div className={classes.user__picture}></div>
+              <div className={classes.user__name}></div>
+            </div>
+            <div className={classes.user__message}>
+              <p>im your Friends</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
