cc51758d2e3c6d37ba30a8d92bfe2c27b5119613
yaumamyfront-ender <nepobedimyjpobedit@gmail.com>
2024-01-31 22:34:24 +0100

    ReduxThunkCreator/fixIssues

--- my-app/src/api/api.js
+++ my-app/src/api/api.js
@@ -4,7 +4,11 @@ let instance = axios.create({
   withCredentials: true,
   baseURL: `https://social-network.samuraijs.com/api/1.0/`,
   headers: {
+<<<<<<< HEAD
     "API-KEY": "ad70deef-0f09-46a9-99ee-7db4f1f66db8",
+=======
+    "API-KEY": "76a5c139-90a9-4a61-a0a4-e862e5f46bfd",
+>>>>>>> bd06db3 (ReduxThunkCreator/fixIssues)
   },
 });
 export let GetUsers = async (Current, Pages) => {
